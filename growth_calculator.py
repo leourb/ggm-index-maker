045ac6acd3ff16e10e4d9ecdbd46fd0ea4369332
Leonardo Urbano <urbanstyle_87@hotmail.it>
2020-12-20 21:09:50 +0100

    Preliminary module

--- growth_calculator.py
+++ growth_calculator.py
@@ -1,2 +1,27 @@
+"""Calculate the growth for all the US stocks"""
+
+from ggm_calculator import InferParameters as IP
+
+from ticker_list import GetTickers
 
 
+class CalculateG:
+    """Calculate g for all the universe of US stocks"""
+
+    def __init__(self):
+        """Initialize the class with the given routines"""
+        self.__tickers = GetTickers().get_downloaded_tickers()
+        self.__growth_parameters = self.__calculate_g()
+
+    def __calculate_g(self):
+        """
+        Calculate the growth parameter for all the tickers
+        :return: a dictionary with the results
+        :rtype: dict
+        """
+        growth_parameters = dict()
+        for letter in self.__tickers:
+            for ticker in self.__tickers[letter]:
+                growth_parameters[ticker] = IP(ticker).get_inferred_g()
+                print(growth_parameters)
+        return growth_parameters
