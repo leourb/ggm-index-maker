d78005b5353b7b3087a176f26bbc379ca4a7df13
Leonardo Urbano <urbanstyle_87@hotmail.it>
2020-12-22 11:08:36 +0100

    Added pickle data

--- growth_calculator.py
+++ growth_calculator.py
@@ -1,6 +1,8 @@
 """Calculate the growth for all the US stocks"""
 
-from ggm_calculator import InferParameters as IP
+import pickle
+
+from ggm_calculator import InferParameters as Ip
 
 from ticker_list import GetTickers
 
@@ -12,6 +14,7 @@ class CalculateG:
         """Initialize the class with the given routines"""
         self.__tickers = GetTickers().get_downloaded_tickers()
         self.__growth_parameters = self.__calculate_g()
+        pickle.dump(self.__growth_parameters, open("ticker_data.pickled", "wb"))
 
     def __calculate_g(self):
         """
@@ -22,6 +25,14 @@ class CalculateG:
         growth_parameters = dict()
         for letter in self.__tickers:
             for ticker in self.__tickers[letter]:
-                growth_parameters[ticker] = IP(ticker).get_inferred_g()
-                print(growth_parameters)
+                growth_parameters[ticker] = Ip(ticker).get_inferred_g()
+                print(growth_parameters[ticker])
         return growth_parameters
+
+    def get_growth_rates(self):
+        """
+        Return the inferred growth rates
+        :return: a dictionary with the growth rates
+        :rtype: dict
+        """
+        return self.__growth_parameters
