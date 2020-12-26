c08c994232421af13c10a921327de6913b1e449b
Leonardo Urbano <urbanstyle_87@hotmail.it>
2020-12-26 17:20:52 +0100

    Module to store static data

---
+++ datashelf.py
@@ -0,0 +1,21 @@
+"""Stores static data"""
+
+MY_LIST = my_list = ['AAPL', 'AMGN', 'AXP', 'BA', 'CAT', 'CRM', 'CSCO', 'CVX', 'DIS', 'DOW', 'GS', 'HD', 'HON', 'IBM',
+                     'INTC', 'JNJ', 'JPM', 'KO', 'MCD', 'MMM', 'MRK', 'MSFT', 'NKE', 'PG', 'TRV', 'UNH', 'V', 'VZ',
+                     'WBA', 'WMT']
+
+
+class DataShelf:
+    """Static data container"""
+
+    def __init__(self):
+        """Initialize the class with the static data"""
+        self.__ticker_list = MY_LIST
+
+    def get_ticker_list(self):
+        """
+        Return the saved ticker list
+        :return: the saved ticker list
+        :rtype: list
+        """
+        return self.__ticker_list
