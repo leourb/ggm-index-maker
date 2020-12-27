f85f3bf8b80b73e6352e5c44d79129e3e0e616e0
Leonardo Urbano <urbanstyle_87@hotmail.it>
2020-12-27 12:06:20 +0100

    New module to calculate growth and weights

---
+++ component_weights.py
@@ -0,0 +1,50 @@
+"""Component Weights and Growth calculator"""
+
+import pickle
+
+import pandas as pd
+
+from scipy.stats import norm
+
+from datashelf import DataShelf
+from growth_calculator import CalculateG
+
+
+class GrowthAndWeights:
+    """Class to calculate the growth and weights for the stocks in input"""
+
+    def __init__(self):
+        """Initialize the class with the given inputs"""
+        self.__ticker_list = DataShelf().get_ticker_list()
+        # self.__growth_rates = CalculateG(self.__ticker_list).get_growth_rates()
+        self.__growth_rates = pickle.load(open("growth_rates.pickled", "rb"))
+        self.__component_weights = self.__calculate_component_weight()
+
+    def __calculate_component_weight(self):
+        """
+        Calculate the component weights to construct a Zero-Investment Portfolio
+        :return: a Pandas DataFrame with the calculated weights
+        :rtype: pd.DataFrame
+        """
+        growth_df = pd.DataFrame(index=list(self.__growth_rates.keys()), data={"g": list(self.__growth_rates.values())})
+        growth_df.dropna(inplace=True)
+        growth_df["z-score"] = (growth_df["g"] - growth_df["g"].mean()) / growth_df["g"].std()
+        growth_df["cdf"] = norm.cdf(growth_df["z-score"])
+        growth_df["weights"] = growth_df["cdf"] - 0.5
+        return growth_df
+
+    def get_ticker_list(self):
+        """
+        Get ticker list as a public member
+        :return: a list of tickers
+        :rtype: list
+        """
+        return self.__ticker_list
+
+    def get_component_weights(self):
+        """
+        Get the component weights as a public member
+        :return: a Pandas DataFrame with the weights
+        :rtype: pd.DataFrame
+        """
+        return self.__component_weights
