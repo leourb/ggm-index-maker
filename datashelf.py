811c775221e6b40a9aa51c9c5ecfe1a468dcdeab
Leonardo Urbano <urbanstyle_87@hotmail.it>
2020-12-27 12:27:59 +0100

    Fixed indentantion

--- datashelf.py
+++ datashelf.py
@@ -1,8 +1,8 @@
 """Stores static data"""
 
 MY_LIST = ['AAPL', 'AMGN', 'AXP', 'BA', 'CAT', 'CRM', 'CSCO', 'CVX', 'DIS', 'DOW', 'GS', 'HD', 'HON', 'IBM',
-                     'INTC', 'JNJ', 'JPM', 'KO', 'MCD', 'MMM', 'MRK', 'MSFT', 'NKE', 'PG', 'TRV', 'UNH', 'V', 'VZ',
-                     'WBA', 'WMT']
+           'INTC', 'JNJ', 'JPM', 'KO', 'MCD', 'MMM', 'MRK', 'MSFT', 'NKE', 'PG', 'TRV', 'UNH', 'V', 'VZ',
+           'WBA', 'WMT']
 
 
 class DataShelf:
